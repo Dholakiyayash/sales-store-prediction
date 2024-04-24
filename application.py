d57b6be7bdee1a53225be47eddd8f4b093d9d478
Dholakiyayash <yashdholakiya2003@gmail.com>
2024-04-24 11:00:35 +0530

    change in home page

--- application.py
+++ application.py
@@ -30,33 +30,21 @@ def predict_datapoint():
 
     if request.method=='POST':
         Item_Weight=float(request.form.get('Item_Weight'))
-        print(Item_Weight)
         Item_Fat_Content=str(request.form.get('Item_Fat_Content'))
-        print(Item_Fat_Content)
         Item_Visibility=float(request.form.get('Item_Visibility'))
-        print(Item_Visibility)
         Item_Type=str(request.form.get('Item_Type'))
-        print(Item_Type)
         Item_MRP=float(request.form.get('Item_MRP'))
-        print(Item_MRP)
         Outlet_Identifier=str(request.form.get('Outlet_Identifier'))
-        print(Outlet_Identifier)
         Outlet_Establishment_Year=int(request.form.get('Outlet_Establishment_Year'))
-        print(Outlet_Establishment_Year)
         Outlet_Size=str(request.form.get('Outlet_Size'))
-        print(Outlet_Size)
         Outlet_Location_Type=str(request.form.get('Outlet_Location_Type'))
-        print(Outlet_Location_Type)
         Outlet_Type=str(request.form.get('Outlet_Type'))
-        print(Outlet_Type)
-        # Item_Outlet_Sales=float(request.form.get('Item_Outlet_Sales'))
 
         Item_Type=int(Item_Type_encoder.transform([[Item_Type]])[0])
         Item_Fat_Content=int(Item_Fat_Content_encoder.transform([[Item_Fat_Content]])[0][0])
         Outlet_Size=int(Outlet_Size_encoder.transform([[Outlet_Size]])[0][0])
         Outlet_Location_Type=int(Outlet_Location_Type_encoder.transform([[Outlet_Location_Type]])[0][0])
         Outlet_Type=int(Outlet_Type_encoder.transform([[Outlet_Type]])[0][0])
-        # Outlet_Identifier=int(Outlet_Identifier_encoder.transform([[Outlet_Identifier]])[0][0])
         
         
 
